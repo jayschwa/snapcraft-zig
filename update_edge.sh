b90d9867310560e1ca082d1b7b1d8eb37e4c195c
Jay Weisskopf <jay@jayschwa.net>
2019-03-16 03:24:54 -0400

    Limit build memory to 300M

--- update_edge.sh
+++ update_edge.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-snap=zig.snap
+# Query latest Zig version from URL
 master=https://s3.amazonaws.com/ziglang.org/builds/zig-linux-x86_64-master.tar.xz
 location=$(curl --head --silent $master | grep location | tr -d '\r\n' | awk '{print $2}')
 version=$(basename $location .tar.xz | cut -c18-)
@@ -12,9 +12,13 @@ if [ "$version" = "$(cat last_build)" ]; then
 	exit
 fi
 
+# Setup working directory
 git clean --force -x --exclude='last_build'
 sed --in-place "s/version: .*/version: $version/" snapcraft.yaml
-snapcraft snap --output $snap
-snapcraft push $snap --release edge
+
+# Build the snap
+export SNAPCRAFT_BUILD_ENVIRONMENT_MEMORY=300M
+snapcraft snap --output zig.snap
+snapcraft push zig.snap --release edge
 
 echo $version > last_build
