4a44d765e4cdb3e511ac3913625b4c1a54fcc2c0
Jay Weisskopf <jay@jayschwa.net>
2019-03-16 01:08:17 -0400

    Read version from uncached S3 URL

--- update_edge.sh
+++ update_edge.sh
@@ -1,18 +1,20 @@
 #!/bin/sh -e
 
 snap=zig.snap
-master=https://ziglang.org/builds/zig-linux-x86_64-master.tar.xz
-current=$(curl --head --silent $master | grep location | awk '{print $2}')
+master=https://s3.amazonaws.com/ziglang.org/builds/zig-linux-x86_64-master.tar.xz
+location=$(curl --head --silent $master | grep location | tr -d '\r\n' | awk '{print $2}')
+version=$(basename $location .tar.xz | cut -c18-)
 
 cd $(dirname $0)
 
-if [ "$current" = "$(cat last_build)" ]; then
+if [ "$version" = "$(cat last_build)" ]; then
 	echo "no change since last build"
 	exit
 fi
 
 git clean --force -x --exclude='last_build'
+sed --in-place "s/version: .*/version: $version/" snapcraft.yaml
 snapcraft snap --output $snap
 snapcraft push $snap --release edge
 
-echo $current > last_build
+echo $version > last_build
