46ddd1db6e2b7a2e03cd31bf00ed323f570005f1
Jay Weisskopf <jay@jayschwa.net>
2019-04-05 00:10:28 -0400

    Query build info from ziglang.org JSON

--- update_edge.sh
+++ update_edge.sh
@@ -6,27 +6,25 @@ if [ "$(pgrep --count --full $0)" -gt 3 ]; then
 	exit
 fi
 
-# Query latest Zig version from URL
-master=https://s3.amazonaws.com/ziglang.org/builds/zig-linux-x86_64-master.tar.xz
-location=$(curl --head --silent $master | grep location | tr -d '\r\n' | awk '{print $2}')
-version=$(basename $location .tar.xz | cut -c18-)
+# Query latest Zig build
+tarball=$(curl https://ziglang.org/download/index.json | jq --raw-output '.master."x86_64-linux".tarball')
 
 cd $(dirname $0)
 
-if [ "$version" = "$(cat last_build)" ]; then
-	echo "No change since last build $version"
+if [ "$tarball" = "$(cat last_build)" ]; then
+	echo "No change since last build ($tarball)"
 	exit
 fi
 
-echo "Building $version"
+echo "Snapping $tarball"
 
 # Setup working directory
-git clean --force -x --exclude='last_build'
-sed --in-place "s/version: .*/version: $version/" snapcraft.yaml
+git clean --force -x --exclude=last_build
+sed --in-place "s,source: .*,source: $tarball," snapcraft.yaml
 
 # Build the snap
 export SNAPCRAFT_BUILD_ENVIRONMENT_MEMORY=300M
 snapcraft snap --output zig.snap
 snapcraft push zig.snap --release edge
 
-echo $version > last_build
+echo $tarball > last_build
