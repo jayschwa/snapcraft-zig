507e75803de1f516a40a3a71c41bc8bd1d3bf284
Jay Petacat <jay@jayschwa.net>
2020-11-10 22:33:29 -0500

    Support tagged releases and missing master branch

--- update_edge.sh
+++ update_edge.sh
@@ -1,20 +1,31 @@
 #!/bin/sh -e
 
+# Tagged versions are built with command line argument.
+version=$1
+
 # Prevent concurrent builds.
 # Three processes are expected when executed via crontab.
 if [ "$(pgrep --count --full $0)" -gt 3 ]; then
 	exit
 fi
 
-# Query latest Zig build.
-download_json=$(curl --silent https://ziglang.org/download/index.json)
-version=$(echo $download_json | jq --raw-output '.master.version')
-
 cd $(dirname $0)
 
-if [ "$version" = "$(cat last_build)" ]; then
-	echo "No change since last build ($version)"
-	exit
+if [ -n "$version" ]; then
+	zig_release=$version
+else
+	# Query latest Zig build from master branch.
+	download_json=$(curl --silent https://ziglang.org/download/index.json)
+	zig_release=master
+	version=$(echo $download_json | jq --raw-output ".\"$zig_release\".version")
+
+	if [ "$version" = "null" ]; then
+		echo "No master branch info"
+		exit
+	elif [ "$version" = "$(cat last_build)" ]; then
+		echo "No change since last build ($version)"
+		exit
+	fi
 fi
 
 # Clean working directory.
@@ -24,7 +35,7 @@ git clean --force -x --exclude=last_build
 for pair in "aarch64 arm64" "armv7a armhf" "i386 i386" "x86_64 amd64"; do
 	zig_arch=$(echo $pair | awk '{print $1}')
 	deb_arch=$(echo $pair | awk '{print $2}')
-	tarball=$(echo $download_json | jq --raw-output ".master.\"$zig_arch-linux\".tarball")
+	tarball=$(echo $download_json | jq --raw-output ".\"$zig_release\".\"$zig_arch-linux\".tarball")
 	[ "$tarball" = null ] && continue
 	echo "Creating $deb_arch snap from $tarball"
 	snap=$(./snaphack $version $deb_arch $tarball | grep --only-matching '[[:graph:]]\+\.snap')
